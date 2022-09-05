9b899e4511eb1104404a208f11a0844c7258c010
v1ktop <ingv1ktop@gmail.com>
2022-09-05 15:06:34 -0500

    Pass data to a child component

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  constructor() {}
+
+  //Indicates that the property value passes in from the component's parent
+  @Input() product!: Product;
+
+  ngOnInit(): void {}
+}
